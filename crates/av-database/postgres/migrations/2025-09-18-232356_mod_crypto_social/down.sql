bbfe970cb0dbc44f176e56704083743a95acca05
Dwight J. Browne <dwight@dwightjbrowne.com>
2025-10-17 21:52:48 -0700

    Initial DB changes for crypto data sourcing

--- crates/av-database/postgres/migrations/2025-09-18-232356_mod_crypto_social/down.sql
+++
@@ -1,7 +0,0 @@
--- This file should undo anything in `up.sql`
--- Remove the index
-DROP INDEX IF EXISTS idx_crypto_social_blockchain_sites;
-
--- Remove the column
-ALTER TABLE crypto_social
-    DROP COLUMN blockchain_sites;
